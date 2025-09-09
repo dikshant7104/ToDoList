e3aa02fe7ec7dee4bcf46e17ebd018df04e4d8a3
x22193146 <x22193146@student.ncirl.ie>
2025-09-09 22:42:28 +0100

    Controlled Component

--- src/Components/Input.tsx
+++ src/Components/Input.tsx
@@ -11,20 +11,20 @@ interface Props{
 }
 export const InputData = ({dispatch}: Props) =>{
 
-    const inputRef = useRef<HTMLInputElement>(null)
+
     const [input, setInput] = useState('');
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
        if (input.trim()) {
             dispatch(addTodoAction(input));
             setInput('');
-            inputRef.current?.blur();
+           
         }
     }
     return <>
         <div>
             <form action="" className="input" onSubmit={handleSubmit}> 
-                <input ref={inputRef}
+                <input 
                  type="input" placeholder="Enter your task" className="input_field"
                 value={input} onChange={(e)=>setInput(e.target.value)} />
             <Button type="submit" className="sub_button" color="primary"><CgSandClock /></Button>
