9213e8e218bac3480bd3114963f2259cf9100a10
BokyaBhau <shivambhuadi@gmail.com>
2025-09-17 00:08:31 +0200

    add clean code setup

--- src/Components/Input.tsx
+++ src/Components/Input.tsx
@@ -1,35 +1,39 @@
-import React,{useRef,useState} from "react";
-import './styles.css'
-import { Button } from 'reactstrap';
+import React, { useState } from "react";
+import "./styles.css";
+import { Button } from "reactstrap";
 import { CgSandClock } from "react-icons/cg";
 import { addTodoAction } from "./list";
-import type { Actions } from './Reducer';
+import type { Actions } from "./Reducer";
 
-interface Props{
-    // todos: string;
-    dispatch : (action: Actions) => void;
+interface Props {
+  // todos: string;
+  dispatch: (action: Actions) => void;
 }
-export const InputData = ({dispatch}: Props) =>{
-
-
-    const [input, setInput] = useState('');
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-       if (input.trim()) {
-            dispatch(addTodoAction(input));
-            setInput('');
-           
-        }
+export const InputData = ({ dispatch }: Props) => {
+  const [input, setInput] = useState("");
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (input.trim()) {
+      dispatch(addTodoAction(input));
+      setInput("");
     }
-    return <>
-        <div>
-            <form action="" className="input" onSubmit={handleSubmit}> 
-                <input 
-                 type="input" placeholder="Enter your task" className="input_field"
-                value={input} onChange={(e)=>setInput(e.target.value)} />
-            <Button type="submit" className="sub_button" color="primary"><CgSandClock /></Button>
-            </form>
-
-        </div>
+  };
+  return (
+    <>
+      <div>
+        <form action="" className="input" onSubmit={handleSubmit}>
+          <input
+            type="input"
+            placeholder="Enter your task"
+            className="input_field"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <Button type="submit" className="sub_button" color="primary">
+            <CgSandClock />
+          </Button>
+        </form>
+      </div>
     </>
-}
+  );
+};
